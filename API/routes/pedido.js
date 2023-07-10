a64f598810313f9c10adf3341e70d4bac3e61ef4
Ana Paula Althoff <anapaula.althoff@gmail.com>
2023-07-10 20:50:08 -0300

    pedido's functions created

--- API/routes/pedido.js
+++ API/routes/pedido.js
@@ -4,6 +4,7 @@ const controllers = require('../controllers/pedido')
 
 // Endpoints
 
+router.post('/criar_pedido', controllers.criar_pedido);
 router.get('/buscar_pedidos', controllers.buscar_pedidos);
 router.get('/consultar_pedidos_usuario/:id_usuario', controllers.consultar_pedidos_usuario);
 
